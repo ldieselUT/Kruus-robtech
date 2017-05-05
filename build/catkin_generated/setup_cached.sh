7422a671f8dcbb057098e02a555f686ff6b32680
tudeng <ldiesel@ut.ee>
2017-05-05 10:19:18 +0300

    last commit

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu:/home/tudeng/Kruus-robtech/devel/lib:/opt/ros/indigo/lib"
-export PKG_CONFIG_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/tudeng/Kruus-robtech/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/tudeng/Kruus-robtech/build"
